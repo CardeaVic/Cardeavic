85c1440ef4a88a17b92240acc29ebe92671258e2
wshu7 <wshu7@student.monash.edu>
2020-04-25 13:56:11 +1000

    Add password in welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -14,7 +14,9 @@
                 <p class="single-text-center">Welcome</p>
                 <i class="fas fa-hand-holding-heart fa-4x" style="color: white"></i>
                 <p class="small-text-center">You are one step away from knowing your heart score!</p>
-                <button type="button" class="w3-round custom-button" onclick="window.location.href='assessment'">Continue</button>
+                <input class="w3-input w3-border-0 w3-round custom-button" placeholder="Password" type="text" id="pw" name="password" style="margin: auto;margin-bottom: 20px;">
+                <button type="button" class="w3-round custom-button" onclick="checkPass()">Continue</button>
+                <p id="errorMsg" style="color:red; visibility:hidden"></p>
             </div>
         </div>
         <ul class="bg-bubbles">
@@ -30,4 +32,19 @@
             <li></li>
         </ul>
     </div>
+    <script>
+        function checkPass(){
+            usrIn = document.getElementById("pw").value;
+            if(usrIn != "123456") {
+                document.getElementById("pw").value = "";
+                document.getElementById("errorMsg").innerHTML = "Invalid input";
+                document.getElementById("errorMsg").style.visibility = "visible";
+                return false;
+            }else{
+                window.location.href = "assessment";
+                return true;
+            }
+        }
+
+    </script>
 @endsection
