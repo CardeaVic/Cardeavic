516c4c1a5882054616cbbe979c7c999ba29170e6
wshu7 <wshu7@student.monash.edu>
2020-05-07 15:59:50 +1000

    Fixed logout

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -4,13 +4,10 @@
                                                   style="width:auto;height:34px"></a>
     <a href="/home" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small">Home</a>
     <a href="/daily-activities/create" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small">Add Activity</a>
-    <a href="javascript:void(0)"
-       class="w3-bar-item w3-button w3-xlarge w3-hover-teal w3-right w3-hide-large w3-hide-medium"
-       onclick="myFunction()">&#9776;</a>
     <a href="logout" class="w3-right">
         <form method="POST" action="{{ route('logout') }}">
             @csrf
-            <a class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small w3-right" type="submit">Logout</a>
+            <button class="w3-bar-item w3-button w3-hover-teal w3-text-white w3-xlarge w3-hide-small w3-right" type="submit">Logout</button>
         </form>
     </a>
 </div>
