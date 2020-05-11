cf1e95696749566b0a8cc74aacf1fabb67094b1d
wshu7 <wshu7@student.monash.edu>
2020-05-11 17:49:45 +1000

    Navbar menu dropdown list

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,9 +1,16 @@
 {{-- Navigation bar --}}
-<div class="w3-bar custom-nav navbar-fixed-top">
+<div class="w3-bar custom-nav">
     <a href="/daily-activities" class="w3-bar-item "><img src="/images/CardeaLogoSmall.png" alt="Cardea Logo"
                                                   style="width:auto;height:34px"></a>
     <a href="/home" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small">Home</a>
-    <a href="/daily-activities" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small">Daily log</a>
+    
+    <div class="w3-dropdown-hover">
+        <button class="w3-button w3-hover-teal w3-xlarge w3-hide-small">Daily log <i class="fa fa-caret-down"></i></button>
+        <div class="w3-dropdown-content w3-bar-block w3-card-4">
+          <a href="/daily-activities" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small">View log</a>
+          <a href="/daily-activities/create" class="w3-bar-item w3-button w3-hover-teal w3-xlarge w3-hide-small ">Add activity</a>
+        </div>
+    </div>
     <a href="javascript:void(0)"
        class="w3-bar-item w3-button w3-xlarge w3-hover-teal w3-right w3-hide-large w3-hide-medium"
        onclick="myFunction()">&#9776;</a>
