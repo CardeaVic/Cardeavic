961f8fa41c2781f687dca1a3454ed27c6b2a9ee4
Subodh Sharma <subodh@mlhs.com.au>
2020-05-15 08:45:21 +0000

    Regiter error message changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'email' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', 'confirmed'],
+        ],[
+            'email.unique' => 'The username has already been taken.',
         ]);
     }
 
