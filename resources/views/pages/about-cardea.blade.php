786c00740579a710ed49443e6cbd260b6bfb38ca
wshu7 <wshu7@student.monash.edu>
2020-05-17 01:00:15 +1000

    Fixed texts in result page

--- resources/views/pages/about-cardea.blade.php
+++ resources/views/pages/about-cardea.blade.php
@@ -20,38 +20,38 @@
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-lg-8">
-              <div id="carouselExampleCaptions" class="carousel slide z-index-2" data-ride="carousel">
+              <div id="carouselCaptions" class="carousel slide z-index-2" data-ride="carousel">
                   <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    <li data-target="#carouselCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselCaptions" data-slide-to="2"></li>
                   </ol>
                   <div class="carousel-inner">
                     <div class="carousel-item active">
-                      <img src="/images/carousel/carousel1.jpg" class="d-block w-100" alt="...">
+                      <img src="/images/carousel/carousel1.png" class="d-block w-100" alt="...">
                       <div class="carousel-caption d-none d-md-block" style="background-color: rgba(77, 77, 77, 0.5)">
                         <p>Personalized <strong>DASHBOARD</strong> for tracking the trends in daily habits.
                         </p>
                       </div>
                     </div>
                     <div class="carousel-item">
-                      <img src="/images/carousel/carousel2.jpg" class="d-block w-100" alt="...">
+                      <img src="/images/carousel/carousel2.png" class="d-block w-100" alt="...">
                       <div class="carousel-caption d-none d-md-block" style="background-color: rgba(77, 77, 77, 0.5)">
                         <p><strong>LOG BOOK</strong> records lifestyle habits primarily focuses on eating habits, physical activity and smoking status.</p>
                       </div>
                     </div>
                     <div class="carousel-item">
-                      <img src="/images/carousel/carousel3.jpg" class="d-block w-100" alt="...">
+                      <img src="/images/carousel/carousel3.png" class="d-block w-100" alt="...">
                       <div class="carousel-caption d-none d-md-block" style="background-color: rgba(77, 77, 77, 0.5)">
                         <p>Generate personalized <strong>REPORTS</strong> about Cardiovascular Health.</p>
                       </div>
                     </div>
                   </div>
-                  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev" style="background-image: linear-gradient(to right, rgba(77,77,77,1), rgba(77,77,77,0));">
+                  <a class="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev" style="background-image: linear-gradient(to right, rgba(77,77,77,1), rgba(77,77,77,0));">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                   </a>
-                  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next" style="background-image: linear-gradient(to right, rgba(77,77,77,0), rgba(77,77,77,1));">
+                  <a class="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next" style="background-image: linear-gradient(to right, rgba(77,77,77,0), rgba(77,77,77,1));">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                   </a>
