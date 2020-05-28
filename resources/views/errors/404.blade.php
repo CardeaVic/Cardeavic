636bc8ed204293b9726bfb88e4ae6b024992df92
Subodh Sharma <subodh@mlhs.com.au>
2020-05-28 07:14:05 +0000

    404 error message changed

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,5 +2,4 @@
 
 @section('title', __('Not Found'))
 @section('code', '404')
-@section('message', 'Sorry, but the page you are looking for has not been found. Try checking the URL for an error,
-then hit the refresh button on your browser or try to find something else in our app.')
+@section('message', 'Sorry, but the page you are looking for has not been found. Continue your journey by visiting the below pages.')
